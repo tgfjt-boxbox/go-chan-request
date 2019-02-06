d2a18cb2c1654cf210a6b39a8d0d5c8f771149b7
tgfjt <tgfjt.mail@gmail.com>
2019-02-06 20:12:48 +0900

    fix scope on client.go

--- utils/client.go
+++ utils/client.go
@@ -5,10 +5,10 @@ import (
 	"sync"
 )
 
-func GetClient() *http.Client {
-	var once sync.Once
-	var c *http.Client
+var once sync.Once
+var c *http.Client
 
+func GetClient() *http.Client {
 	once.Do(func() {
 		c = &http.Client{}
 	})
